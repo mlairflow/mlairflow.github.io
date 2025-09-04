3f820085939d2b7ff6b1dede998238169f8e1537
MichaelKarpe <28049739+MichaelKarpe@users.noreply.github.com>
2025-09-04 12:27:54 +0000

    Deploying to gh-pages from @ mlairflow/mlairflow.github.io@18ee7fbd6845844af041229f90863f7b24e682e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
